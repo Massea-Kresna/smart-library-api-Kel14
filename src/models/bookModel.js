c6c801c2712df718d9b6041f123f6ddfbeecfb23
Massea-Kresna <masseakresnaaravenaa@gmail.com>
2026-04-21 01:16:06 +0700

    feat(book): add getById, and update

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,7 +1,6 @@
 import { pool } from '../config/db.js';
 
-export const BookModel = {
-  // Mengambil semua buku dengan nama penulis dan kategori (JOIN)
+export const BookModel = {//mengambil semua buku dengan nama penulis dan kategori (JOIN)
   async getAll() {
     const query = `
       SELECT b.*, a.name as author_name, c.name as category_name 
@@ -13,7 +12,19 @@ export const BookModel = {
     return result.rows;
   },
 
-  async create(data) {
+  async getById(id) {//mengambil satu buku berdasarkan id (dengan JOIN)
+    const query = `
+      SELECT b.*, a.name as author_name, c.name as category_name 
+      FROM books b
+      LEFT JOIN authors a ON b.author_id = a.id
+      LEFT JOIN categories c ON b.category_id = c.id
+      WHERE b.id = $1      
+    `;
+    const result = await pool.query(query, [id]);
+    return result.rows[0];
+  },
+
+  async create(data) {//menambah buku
     const { isbn, title, author_id, category_id, total_copies } = data;
     const query = `
       INSERT INTO books (isbn, title, author_id, category_id, total_copies, available_copies)
@@ -23,7 +34,19 @@ export const BookModel = {
     return result.rows[0];
   },
 
-  async delete(id) {
+  async update(data) { //memperbarui buku
+    const { isbn, title, author_id, category_id, total_copies } = data;
+    const query = `
+      UPDATE books 
+      SET isbn = $1, title = $2, author_id = $3, category_id = $4, total_copies = $5
+      WHERE id = $6 
+      RETURNING *
+    `;
+    const result = await pool.query(query, [isbn, title, author_id, category_id, total_copies, id]);
+    return result.rows[0];
+  },
+
+  async delete(id) {//menghapus buku
     const query = 'DELETE FROM books WHERE id = $1';
     await pool.query(query, [id]);
     return { message: "Buku berhasil dihapus dari sistem." };
