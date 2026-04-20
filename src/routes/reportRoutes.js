2e4a61932f8b3c4f840ab155190cc8cd6a2f7149
Massea-Kresna <masseakresnaaravenaa@gmail.com>
2026-04-21 03:46:07 +0700

    feat (reportRoutes): Defined new API endpoints for reports

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -3,6 +3,6 @@ import { ReportController } from '../controllers/reportController.js';
 
 const router = express.Router();
 
-router.get('/stats', ReportController.getLibraryStats);
+router.get('/stats', ReportController.getStats); //GET /api/reports/stats
 
 export default router;
