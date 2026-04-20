a5894e9d877f288e333b9b28adf4edd6354ad8c2
Massea-Kresna <masseakresnaaravenaa@gmail.com>
2026-04-21 01:16:58 +0700

    feat(category): add getById, update, and delete endpoints

--- src/models/categoryModel.js
+++ src/models/categoryModel.js
@@ -1,13 +1,36 @@
 import { pool } from '../config/db.js';
 
-export const CategoryModel = {
+export const CategoryModel = { //mengambil semua category
   async getAll() {
     const result = await pool.query('SELECT * FROM categories ORDER BY name ASC');
     return result.rows;
   },
-  async create(name) {
+
+  async getById(id) { //mengambil satu category berdasarkan id
+    const result = await pool.query('SELECT * FROM categories WHERE id = $1', [id]);
+    return result.rows[0];
+  },
+
+  async create(name) { //menambah category baru
     const query = 'INSERT INTO categories (name) VALUES ($1) RETURNING *';
     const result = await pool.query(query, [name]);
     return result.rows[0];
+  },
+
+  async update(id, name) { //memperbarui data category
+    const query = `
+      UPDATE categories 
+      SET name = $1
+      WHERE id = $2 
+      RETURNING *
+    `;
+    const result = await pool.query(query, [name, id]);
+    return result.rows[0];
+  },
+  
+  async delete(id) { //menghapus data category
+    const query = 'DELETE FROM categories WHERE id = $1';
+    await pool.query(query, [id]);
+    return { message: "Kategori berhasil dihapus dari sistem." };
   }
 };
